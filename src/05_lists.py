ea9cebea06bd0d3bcf5345d8bfadcf27604412a7
ch04937 <97hernandez.c@gmail.com>
2020-01-13 16:16:03 -0800

    lists complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,28 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+x.append(4)
+print("\n Adds 4 to the end: ", x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+print("\n Combines array x and y: ", x + y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+x.extend((8, 9, 10))
+print("\n add 8, 9, 10 to array x: ", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(6, 99)
+print("\n Add 99 in array x: ", x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print("\n Length of list: ", len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+n = [i * 1000 for i in x]
+print("\n Multiplied by 1000", n)
